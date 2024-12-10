a449d7b555d907a825c46702810c1d46d88baedd
Zainab Alobed <zainabh647@gmail.com>
2024-12-10 20:27:35 +0300

    improve structure of server and commenting

--- server/server.py
+++ server/server.py
@@ -6,23 +6,29 @@ import requests
 import ssl
 import os
 
-# API Key for NewsAPI
+"""  
+savind API key and base url
+"""
 API_KEY = "4abb7da35b8346dfa7f1f20b5bc353e7"
-# newsAPI link
 BASE_URL = "https://newsapi.org/v2"
 
-# for SSL/TLS
+"""
+for SSL/TLS we need to save the certificate and key paths
+"""
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CERT_FILE = os.path.join(BASE_DIR, "../project.crt")
 KEY_FILE = os.path.join(BASE_DIR, "../project.key")
 
 
-# headline search function with filltering
 def get_headlines(option, value):
-    # pass api key
+    """
+    This function is for getting headlined from the endpoint
+    /top-headlines with specific criteria chosed by the client
+    with the using of params
+    """
+
     params = {"apiKey": API_KEY, "pageSize": 15}
 
-    # check choosen option
     if option == "keywords":
         params["q"] = value
     elif option == "category":
@@ -30,10 +36,12 @@ def get_headlines(option, value):
     elif option == "country":
         params["country"] = value
 
-    # get response from endpoint and return it
     try:
         response = requests.get(f"{BASE_URL}/top-headlines", params=params)
         if response.status_code != 200:
+            """
+            if code is not 200, then the request was not accepted
+            """
             print(f"Error with API: {response.status_code}, {response.text}")
             return {"error": "API error"}
         return response.json()
@@ -43,6 +51,9 @@ def get_headlines(option, value):
 
 # all headlines search function
 def get_all_headlines():
+    """
+    All headlines have different endpoint which is /everything
+    """
     params = {"apiKey": API_KEY, "pageSize": 15, "language": "en", "q": "news"}
     try:
         response = requests.get(f"{BASE_URL}/everything", params=params)
@@ -54,12 +65,13 @@ def get_all_headlines():
         return {"API_error": "API error, check the connection"}
 
 
-# resource search function
 def get_sources(option, value):
-    # pass api key
+    """
+    getting the sources from endpoint /sources with the use of
+    params to pass the API key and filtering criteria
+    """
     params = {"apiKey": API_KEY, "pageSize": 15}
 
-    # check chosen option
     if option == "category":
         params["category"] = value
     elif option == "country":
@@ -79,22 +91,58 @@ def get_sources(option, value):
 
 
 def create_file(client_name, response, list, option):
-    # ensre there is no space or special character in client name and file name
+    """
+    ensre there is no space or special character in client name and file name
+    and save the full API response into a json file
+    """
     safe_client_name = re.sub(r"[^\w]", "_", client_name)
     file_name = (
-        f"{safe_client_name}_{list.replace(' ', '_')}-{option.replace(' ','_')}_B4.json"
+        f"{safe_client_name}_{list.replace(' ', '_')}"
+        f"-{option.replace(' ','_')}_B4.json"
     )
     with open(file_name, "w", encoding="utf-8") as file:
         json.dump(response, file, ensure_ascii=False, indent=4)
 
 
-# prepare the list of headlines/sources
-def prepare_list(res, list):
-    res = res[:15]  # only 15 results
+def handle_request(list, option, client_name, value=""):
+    """
+    Checking wether the client is looking for healdlines or
+    sources list and communicate with the approprate function
+    """
+    if list == "headlines":
+        if option == "all":
+            print(f"client {client_name} requested to search for all {list}")
+            full_response = get_all_headlines()
+        else:
+            print(
+                f"client {client_name} requested to search for {list} by {option} ({value})"
+            )
+            full_response = get_headlines(option, value)
+        response = full_response.get("articles", [])
 
+    elif list == "sources":
+        if option == "all":
+            print(f"client {client_name} requested to search for all {list}")
+        else:
+            print(
+                f"client {client_name} requested to search for {list} by {option} ({value})"
+            )
+        full_response = get_sources(option, value)
+        response = full_response.get("sources", [])
+
+    return response, full_response
+
+
+def prepare_list(response, list):
+    """
+    Taking the list of articles/sources and
+    prepare a list with only brief data for
+    only 15 elements
+    """
+    response = response[:15]  # only 15 results
     prepared_list = []
 
-    for x in res:
+    for x in response:
         if list == "headlines":
             prepared_list.append(
                 {
@@ -104,7 +152,7 @@ def prepare_list(res, list):
                 }
             )
 
-        else:  # sources
+        else:
             prepared_list.append({"name": x.get("name", "unknown")})
 
     return prepared_list
@@ -121,15 +169,22 @@ def receive_complete_data(socket):
     return data.decode("utf-8")
 
 
-# main thread
 def search(sock):
+    """
+    This method is to handle connection with the clients
+    """
     try:
-        # recieve client name and print it
+        """
+        recieve client name and print it
+        """
         client_name = receive_complete_data(sock)
         print(f"New client connected {client_name}. Awating requests...")
 
         while True:
-            # get a request from the client
+            """
+            get requests from the clients and response to them within the loop
+            """
+
             try:
                 data = receive_complete_data(sock)
                 data = data.split(",")
@@ -137,70 +192,66 @@ def search(sock):
             except Exception as e:
                 print(f"Error receiving or processing data: {e}")
                 continue
-            # if client choosed to quit
+
             if data[0] == "Quit":
                 break
 
-            # split the id th know the exact option the client choosed
+            """
+            split the request th know the exact option the client choosed
+            """
             list = data[0].strip()  # resource or headline
             option = data[1].strip()  # search with what (category/language....)
 
-            # get the list depending on the client choice headline/source
-            if list == "headlines":
-                # endpoint for all headlines is defferent
-                if option == "all":
-                    print(f"client {client_name} requested to search for all {list}")
-                    response = get_all_headlines()
-                else:
-                    value = data[2].strip()
-                    print(
-                        f"client {client_name} requested to search for {list} by {option} ({value})"
-                    )
-                    response = get_headlines(option, value)
-                res = response.get("articles", [])
+            """ 
+            get the list depending on the client choice headline/source
+            """
+            response, full_response = handle_request(
+                list, option, client_name, data[2].strip() if 2 < len(data) else ""
+            )
 
-            elif list == "sources":
-                if option == "all":
-                    print(f"client {client_name} requested to search for all {list}")
-                    value = ""
-                else:
-                    value = data[2].strip()
-                    print(
-                        f"client {client_name} requested to search for {list} by {option} ({value})"
-                    )
-                response = get_sources(option, value)
-                res = response.get("sources", [])
+            """
+            save the result in json file
+            """
+            create_file(client_name, full_response, list, option)
 
-            # save the result in json file
-            create_file(client_name, response, list, option)
-
-            # If failed to connect the the api
-            if "API_error" in response:
+            """
+            If failed to connect the the api and code was not 200
+            """
+            if "API_error" in full_response:
                 sock.sendall(response["API_error"])
                 continue
 
-            # prepare and send a list to respond, or send no result message
-            if res:
-                prepared_list = prepare_list(res, list)
+            """
+            repare and send a list to respond, or send no result message
+            """
+            if response:
+                prepared_list = prepare_list(response, list)
                 sock.sendall(json.dumps(prepared_list).encode("utf-8"))
 
-            else:  # if no result from API
+            else:
                 sock.sendall("No results found. Please try agaim".encode("utf-8"))
                 continue
 
-            # getting client response after displaying the sources/headline list
+            """
+            After displaying the list to the user in the client, the 
+            user will be able to select an element from the list
+            to get the remaining details or he may mack to the main menue
+            """
             select = receive_complete_data(sock).strip()
 
-            # if the client did not choose a specific headline/source from the list
             if select == "Quit":
                 break
             if select == "back":
                 continue
 
-            # return the article chosen by the client
-            elif select.isdigit() and 1 <= int(select) <= len(res):
+            elif select.isdigit() and 1 <= int(select) <= len(response):
+                """
+                The condition ensure a valid select from user
+                """
                 sock.sendall(
-                    json.dumps(res[int(select) - 1], ensure_ascii=False).encode("utf-8")
+                    json.dumps(response[int(select) - 1], ensure_ascii=False).encode(
+                        "utf-8"
+                    )
                 )
             else:
                 sock.sendall("Sorry! invalid selection.".encode("utf-8"))
@@ -262,7 +313,9 @@ def main():
 
                 print("Server is closing")
     except Exception as e:
-        print(f"Error starting server: Please check the certificate and key files: {e}")
+        print(
+            f"Error starting server: Please check the" " certificate and key files: {e}"
+        )
 
 
 if __name__ == "__main__":
